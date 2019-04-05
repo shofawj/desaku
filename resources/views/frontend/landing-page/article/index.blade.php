a740888ae306397e17d39ace69176a52ad338f1b
iqbalaulia <miqbalar16@gmail.com>
2019-04-05 17:11:06 +0700

    page article clear 80%

--- resources/views/frontend/landing-page/article/index.blade.php
+++ resources/views/frontend/landing-page/article/index.blade.php
@@ -241,7 +241,69 @@
             {{--  --}}
             <div class="col-md-4 col-sm-4">
                 <div class="aside">
-
+                    <div class="sidebar">
+                        <!-- Search Widget -->
+                        <div class="widget search p-0">
+                            <div class="input-group">
+                                <input type="text" class="form-control main m-0" id="expire" placeholder="Search...">
+                                <span class="input-group-addon"><i class="fa fa-search"></i></span>
+                            </div>
+                        </div>
+                        <!-- Category Widget -->
+                        <div class="widget category">
+                            <!-- Widget Header -->
+                            <h5 class="widget-header">Kategori</h5>
+                            <ul class="category-list m-0 p-0">
+                                <li><a href="">Buah - buahan <span class="float-right">(6)</span></a></li>
+                                <li><a href="">Kacang - kacangan <span class="float-right">(9)</span></a></li>
+                                <li><a href="">Sayur - sayuran<span class="float-right">(3)</span></a></li>
+                                <li><a href="">Hewan ternak<span class="float-right">(5)</span></a></li>
+                                <li><a href="">Biji - bijian<span class="float-right">(7)</span></a></li>
+                            </ul>
+                        </div>
+                        <!-- Latest post -->
+                        <div class="widget latest-post">
+                            <h5 class="widget-header">Popular</h5>
+                            <!-- Post -->
+                            <div class="media">
+                                <img src="images/news/post-thumb-sm-one.jpg" class="img-fluid" alt="post-thumb">
+                                <div class="media-body">
+                                    <h6><a href="">Nam hendrerit eros in ligula suscipit suscipit</a></h6>
+                                    <p href="#"><span class="fa fa-calendar"></span>02 Feb, 2017</p>
+                                    <a href="" class="btn btn-primary">Selengkapnya</a>
+                                </div>
+                            </div>
+                            <!-- Post -->
+                            <div class="media">
+                                <img src="images/news/post-thumb-sm-two.jpg" class="img-fluid" alt="post-thumb">
+                                <div class="media-body">
+                                    <h6><a href="">Nam hendrerit eros in ligula suscipit suscipit</a></h6>
+                                    <p href="#"><span class="fa fa-calendar"></span>02 Feb, 2017</p>
+                                    <a href="" class="btn btn-primary">Selengkapnya</a>
+                                </div>
+                            </div>
+                            <!-- Post -->
+                            <div class="media">
+                                <img src="images/news/post-thumb-sm-three.jpg" class="img-fluid" alt="post-thumb">
+                                <div class="media-body">
+                                    <h6><a href="">Nam hendrerit eros in ligula suscipit suscipit</a></h6>
+                                    <p href="#"><span class="fa fa-calendar"></span>02 Feb, 2017</p>
+                                    <a href="" class="btn btn-primary">Selengkapnya</a>
+                                </div>
+                            </div>
+                            <!-- Post -->
+                            <div class="media">
+                                <img src="images/news/post-thumb-sm-four.jpg" class="img-fluid" alt="post-thumb">
+                                <div class="media-body">
+                                    <h6><a href="">Nam hendrerit eros in ligula suscipit suscipit</a></h6>
+                                    <p href="#"><span class="fa fa-calendar"></span>02 Feb, 2017</p>
+                                    <a href="" class="btn btn-primary">Selengkapnya</a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Popular Tag Widget -->
+                       
+                    </div>
                 </div>
             </div>
         </div>
@@ -413,8 +475,6 @@
         width: 100%;
         height: auto;
         padding: 2%;
-
-        border: 1px solid #000000;
     }
 
     .tab-menu {
@@ -474,8 +534,7 @@
 
     .aside {
         width: 100%;
-        height: 100px;
-        border: 1px solid #000000;
+        height: auto;
     }
 
 </style>
