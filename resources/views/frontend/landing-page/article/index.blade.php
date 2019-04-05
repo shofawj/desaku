a1f28cdd84058b5d1aa519683287efc030c14229
iqbalaulia <miqbalar16@gmail.com>
2019-04-05 16:57:19 +0700

    refind css navbar

--- resources/views/frontend/landing-page/article/index.blade.php
+++ resources/views/frontend/landing-page/article/index.blade.php
@@ -15,24 +15,29 @@
 
                                     <main class="primary"
                                         style="background-image: url('http://placekitten.com/410/500') ;"></main>
-                                        
-                                        <div class="thumbnails">
-                                                <a href="#" class="selected thumbnail" data-big="http://placekitten.com/420/600">
-                                                  <div class="thumbnail-image" style="background-image: url(http://placekitten.com/420/600)"></div>
-                                                </a>
-                                                <a href="#" class="thumbnail" data-big="http://placekitten.com/450/600">
-                                                  <div class="thumbnail-image" style="background-image: url(http://placekitten.com/450/600)"></div>
-                                                </a>
-                                                <a href="#" class="thumbnail" data-big="http://placekitten.com/460/700">
-                                                  <div class="thumbnail-image" style="background-image: url(http://placekitten.com/460/700)"></div>
-                                                </a>
-                                                <a href="#" class="thumbnail" data-big="http://placekitten.com/460/700">
-                                                    <div class="thumbnail-image" style="background-image: url(http://placekitten.com/460/700)"></div>
-                                                  </a>
-                                               
-                                              </div>
-                                        
+
+                                    <div class="thumbnails">
+                                        <a href="#" class="selected thumbnail"
+                                            data-big="http://placekitten.com/420/600">
+                                            <div class="thumbnail-image"
+                                                style="background-image: url(http://placekitten.com/420/600)"></div>
+                                        </a>
+                                        <a href="#" class="thumbnail" data-big="http://placekitten.com/450/600">
+                                            <div class="thumbnail-image"
+                                                style="background-image: url(http://placekitten.com/450/600)"></div>
+                                        </a>
+                                        <a href="#" class="thumbnail" data-big="http://placekitten.com/460/700">
+                                            <div class="thumbnail-image"
+                                                style="background-image: url(http://placekitten.com/460/700)"></div>
+                                        </a>
+                                        <a href="#" class="thumbnail" data-big="http://placekitten.com/460/700">
+                                            <div class="thumbnail-image"
+                                                style="background-image: url(http://placekitten.com/460/700)"></div>
+                                        </a>
+
                                     </div>
+
+                                </div>
                             </div>
                             {{-- End --}}
                             {{-- Section  --}}
@@ -119,38 +124,128 @@
 </section>
 
 <section class="description">
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-md-8 col-sm-8">
-            <div class="tab-menu">
-               
-                <div class="tab-wrapper">
-  
-                    <ul class="tab-menu">
-                      <li class="active">Deskripsi</li>
-                      <li>Review</li>
-                     
-                    </ul>
-                    
-                    <div class="tab-content">
-                      <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis exercitationem minima quisquam ex earum sapiente, debitis ipsum quidem optio rerum!</div>
-                    
-                      <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, ad esse. Hic at officiis fuga mollitia quod.</div>
-                    </div><!-- //tab-content -->
-                    
-                  </div><!-- //tab-wrapper -->
-                  
-                  
-            </div>  
-        </div>
-        {{--  --}}
-        <div class="col-md-4 col-sm-4">
-            <div class="aside">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-8 col-sm-8">
+                <div class="tab-menu">
 
+                    <div class="tab-wrapper">
+
+                        <ul class="tab-menu">
+                            <li class="active">Deskripsi</li>
+                            <li>Review</li>
+
+                        </ul>
+
+                        <div class="tab-content">
+                            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto porro culpa
+                                corrupti quos! Consequatur obcaecati laboriosam provident mollitia, ut culpa repellendus
+                                eos quia illum possimus ea quis dolorem nemo, deserunt repudiandae voluptas rerum quae
+                                architecto ducimus illo quasi magni eveniet facere accusamus? Molestiae soluta deleniti
+                                quia eveniet ratione voluptas exercitationem asperiores provident quos quisquam magni
+                                quas maiores, dicta suscipit. Vitae mollitia dolore sit necessitatibus, provident
+                                consequuntur aperiam animi sunt ullam. Porro quisquam dignissimos, voluptate dolores
+                                nemo repellat vitae perspiciatis sapiente praesentium maiores odit iste quaerat nulla
+                                deserunt ratione et deleniti aliquid quibusdam libero facilis voluptates ab? Adipisci
+                                fuga ea debitis, id molestias voluptates. Quae, enim doloremque! Eveniet necessitatibus
+                                assumenda est ad esse consectetur provident error voluptatem, iusto cumque eos quasi
+                                facere hic corrupti maiores veritatis soluta. Soluta fugit magnam, aut animi maiores eum
+                                eveniet sint repudiandae voluptatum vero asperiores. Facilis amet aliquam dolorum
+                                voluptatem deserunt, neque minus earum delectus aperiam, reprehenderit eum possimus
+                                itaque maxime. Similique quisquam molestias delectus accusamus, repellat modi, tempore
+                                provident alias numquam, nemo quas impedit voluptas architecto. At ex architecto harum
+                                vel beatae animi provident. Maxime porro numquam voluptatibus maiores natus nulla ipsa,
+                                et adipisci repellendus mollitia quis quibusdam optio sapiente aut quas quos aliquam
+                                fuga accusamus eum repudiandae tempore suscipit est provident. Quas debitis dignissimos
+                                sequi tenetur? Odit quas mollitia, perferendis excepturi ab ullam perspiciatis vero
+                                incidunt ea sequi quis nam doloremque dolore error non laborum nihil sapiente aliquid
+                                impedit totam officiis consequatur? Praesentium ipsa saepe ducimus alias pariatur rem,
+                                necessitatibus quibusdam quidem unde veniam distinctio repudiandae quos iusto est qui in
+                                perspiciatis. Ipsum corporis eius ad aperiam, sunt assumenda quasi quisquam omnis nisi
+                                quidem amet architecto eos similique debitis, quibusdam velit optio unde nihil esse!
+                                Consectetur laborum repellat ab odio modi obcaecati iusto numquam fuga velit maxime,
+                                ipsam rerum nihil, delectus ea soluta! Laudantium, alias? Quaerat, ipsum esse aperiam
+                                enim, vel facilis iusto laboriosam odio laudantium corrupti vitae eius itaque ratione
+                                eaque odit! Voluptatum nihil et, expedita nemo, corrupti quia dolor maxime tenetur
+                                placeat eveniet vel sint similique, asperiores alias in quos commodi iure iste quod
+                                magni ipsa autem? Qui tenetur, aliquid omnis voluptatum praesentium unde laboriosam
+                                facilis libero blanditiis doloremque in! Expedita suscipit voluptas voluptatum
+                                asperiores labore ullam, recusandae beatae eaque numquam cum harum sequi perspiciatis
+                                inventore voluptate aliquid eos neque repudiandae quas aspernatur quibusdam? Architecto
+                                hic quasi, soluta ut repellendus unde quidem? Architecto unde nihil similique cumque
+                                quae veritatis? Unde, harum nemo! Facere porro, aliquam, cupiditate asperiores modi aut
+                                dolores officia esse sequi eum impedit, obcaecati earum possimus animi totam! Incidunt
+                                assumenda laborum voluptatibus delectus aperiam beatae, voluptates recusandae
+                                dignissimos veritatis? Provident modi sunt accusantium repellendus eaque, perferendis
+                                maiores excepturi placeat dolor fugit nostrum velit voluptatum eligendi hic veniam in
+                                omnis inventore quam ipsam reiciendis? Accusamus cum officiis incidunt. Earum nihil
+                                neque laboriosam eligendi, dignissimos voluptatibus dolore inventore temporibus suscipit
+                                nostrum. Minima, aperiam odit? Vitae provident blanditiis non, maiores magnam soluta
+                                nihil, doloremque harum consectetur recusandae nobis porro earum! Deleniti, eum placeat
+                                culpa nesciunt sapiente, quidem, aperiam est odio qui eveniet quisquam facere blanditiis
+                                voluptatem aliquam dicta dolore quis aliquid at! Aspernatur eos alias ab labore, dolore
+                                expedita velit modi sit veniam delectus aut saepe vel, necessitatibus quisquam
+                                perspiciatis sapiente ut similique placeat maxime maiores! Soluta dicta impedit possimus
+                                sunt inventore sapiente iusto harum explicabo. Enim tempora explicabo voluptatum nemo,
+                                cumque possimus, dolore esse consectetur, voluptates molestias natus voluptatibus ut.
+                                Accusamus assumenda aliquam aut minima at in nesciunt? Voluptate maiores porro vitae
+                                totam fuga, rem unde saepe voluptas qui atque ea officiis neque tenetur autem modi quasi
+                                corrupti rerum error provident odit hic alias nostrum dignissimos quos! Similique iste
+                                accusamus doloribus alias quibusdam illum quia tempora ducimus placeat maxime doloremque
+                                recusandae, amet soluta pariatur, odit aliquid tenetur optio eum? Aliquam asperiores
+                                repellendus voluptate ratione, enim voluptates neque dolore debitis in vel laudantium
+                                placeat reiciendis alias tenetur aspernatur, autem dolorum labore distinctio quas nemo!
+                                Distinctio obcaecati itaque tenetur dignissimos minima facere minus voluptatum
+                                voluptates eligendi quia debitis laudantium odit officia sed deleniti labore culpa
+                                magnam recusandae, ipsum quo voluptatem blanditiis soluta? Repellendus amet quia
+                                perferendis quasi fuga, eveniet ex dolorum fugit maiores consequatur reprehenderit,
+                                nesciunt, qui modi laudantium dicta obcaecati quod ab sunt natus perspiciatis omnis.
+                                Deserunt minima adipisci distinctio! Exercitationem optio voluptas soluta tempora. Sequi
+                                doloremque culpa reprehenderit quae nam beatae officiis, asperiores incidunt corrupti
+                                maiores reiciendis facere minima esse eligendi, cupiditate ab. Odio eveniet facere
+                                cupiditate saepe blanditiis officiis culpa quidem corrupti labore tempore quae
+                                temporibus similique repudiandae soluta necessitatibus eius accusamus alias nostrum,
+                                nihil doloribus. Adipisci quo, consequatur non minima aut assumenda iste distinctio
+                                vero, explicabo harum tempore. Laudantium sit sint, illum magnam voluptates quia
+                                provident culpa vel. Facilis non similique veniam eos cupiditate deserunt, temporibus
+                                unde rem possimus impedit, necessitatibus at labore. Libero voluptatibus, dolorum ab
+                                iusto corrupti nam, optio magni similique aut consectetur fugiat maiores mollitia iste.
+                                Magnam modi quaerat hic magni vel molestiae, accusamus eos ut eaque dolore quo.
+                                Recusandae beatae ab ullam libero molestias aut possimus vitae iusto saepe tenetur
+                                eveniet sapiente sit ut vel autem, fuga natus nobis. Dolorem, deleniti. Porro cum vel
+                                eligendi tempore. Culpa pariatur et quae odio tempore tempora ipsam ullam enim soluta
+                                architecto magni eum, modi commodi magnam nam harum alias! Itaque explicabo eos maxime
+                                dicta veritatis doloremque ipsa corporis sed. Rem enim quae suscipit tempore
+                                praesentium, accusamus, sapiente beatae cumque, corporis quam nobis animi assumenda
+                                veritatis nostrum labore? Repellat, fugiat. Iusto dolores velit repellat similique,
+                                fugiat sint quas libero, facere dignissimos delectus mollitia quibusdam ratione quam,
+                                nulla unde temporibus rem voluptatibus est repellendus! Reprehenderit soluta accusamus
+                                quia, unde iusto sunt totam dolores et accusantium quas, voluptate necessitatibus
+                                ducimus nemo. Eum modi possimus eius quis aspernatur id ratione vel. Sit quibusdam,
+                                sapiente accusantium voluptatibus a dolorem ducimus doloremque aperiam tempore mollitia
+                                fugiat repellendus, quisquam aliquid ab quia cupiditate hic consectetur? Aliquam, veniam
+                                asperiores similique neque, ipsum quo vero enim commodi ipsa culpa cumque modi quisquam
+                                illum voluptate inventore laudantium earum perspiciatis, et delectus optio. Laudantium
+                                odio rerum unde! Veniam, voluptas voluptatum dolorum nam perferendis itaque laudantium
+                                repellat voluptatem porro ducimus pariatur!</div>
+
+                            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, ad esse. Hic at
+                                officiis fuga mollitia quod.</div>
+                        </div><!-- //tab-content -->
+
+                    </div><!-- //tab-wrapper -->
+
+
+                </div>
+            </div>
+            {{--  --}}
+            <div class="col-md-4 col-sm-4">
+                <div class="aside">
+
+                </div>
             </div>
         </div>
     </div>
-</div>
 </section>
 
 @endsection
@@ -163,7 +258,8 @@
         width: 100%;
         height: auto;
         padding: 2%;
-a    }
+        a
+    }
 
     .banner-page .card-banner {
         width: 100%;
@@ -291,8 +387,8 @@ a    }
         border: 1px solid #000000;
         background-color: #cccccc;
         background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
+        background-repeat: no-repeat;
+        background-size: cover;
     }
 
     .thumbnail:hover .thumbnail-image,
@@ -313,75 +409,75 @@ a    }
 
     /* Section description */
 
-    .description{
+    .description {
         width: 100%;
         height: auto;
         padding: 2%;
 
-        border:1px solid #000000;
+        border: 1px solid #000000;
     }
-    
-    .tab-menu{
+
+    .tab-menu {
         width: 100%;
         height: auto;
 
     }
 
     .tab-wrapper {
-  width:100%;
-  
-}
+        width: 100%;
 
-.tab-menu li {
-  position:relative;
-  background-color: #fff;
-  color:#bcbcbc;
-  display: inline-block;
-  padding: 20px 40px;
-  opacity: 0.8;
-  cursor:pointer;
-  z-index:0;
-}
+    }
 
-.tab-menu li:hover {
-  color:#464646;
-}
+    .tab-menu li {
+        position: relative;
+        background-color: #fff;
+        color: #bcbcbc;
+        display: inline-block;
+        padding: 20px 40px;
+        opacity: 0.8;
+        cursor: pointer;
+        z-index: 0;
+    }
 
-.tab-menu li.active {
-  color:#464646;
-  opacity: 1;
-}
+    .tab-menu li:hover {
+        color: #464646;
+    }
 
-.tab-menu li.active:hover {
-  color:#464646;
-}
+    .tab-menu li.active {
+        color: #464646;
+        opacity: 1;
+    }
 
-.tab-content>div {
-  background-color: #fff;
-  box-sizing:border-box;
-  width: 100%;
-  padding: 50px;   
-  min-height:200px;
-}
+    .tab-menu li.active:hover {
+        color: #464646;
+    }
 
-.line {
-  width: 0;
-  position:absolute;
-  height: 7px;
-  background-color: #e67e22;
-  top: 0;
-  left: 0;
-}
+    .tab-content>div {
+        background-color: #fff;
+        box-sizing: border-box;
+        width: 100%;
+        padding: 50px;
+        min-height: 200px;
+    }
 
-
+    .line {
+        width: 0;
+        position: absolute;
+        height: 7px;
+        background-color: #e67e22;
+        top: 0;
+        left: 0;
+    }
 
 
-    .aside{
+
+
+    .aside {
         width: 100%;
         height: 100px;
         border: 1px solid #000000;
     }
+
 </style>
 
 @endpush
-
