4d8d7de137857d87b69742ccffe98b4ef13134e3
iqbalaulia <miqbalar16@gmail.com>
2019-04-05 13:04:23 +0700

    js image click in banner page article clear

--- resources/views/frontend/landing-page/article/index.blade.php
+++ resources/views/frontend/landing-page/article/index.blade.php
@@ -280,13 +280,3 @@ a    }
 
 @endpush
 
-<script>
-        $('.thumbnail').on('click', function() {
-          let clicked = $(this);
-          let newSelection = clicked.data('big');
-          let $img = $('.primary').css("background-image","url(" + newSelection + ")");
-          clicked.parent().find('.thumbnail').removeClass('selected');
-          clicked.addClass('selected');
-          $('.primary').empty().append($img.hide().fadeIn('slow'));
-        });
-        </script>
