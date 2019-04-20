fdf28014562cfdcc8e0ca63aaef906217bce3452
iqbalaulia <miqbalar16@gmail.com>
2019-04-21 03:13:08 +0700

    fix problem seeder

---
+++ app/Model/product.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Desaku\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $table= 'products';
+    protected $gruarderd= ['id'];
+}
