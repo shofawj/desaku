571dcbcf6fea77af2957d411523e53512a8807da
shofawj <shofapw@gmail.com>
2020-02-18 11:49:59 +0700

    hai ko

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,6 +11,12 @@
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
+
+
+
+
+
+                            
                         </div>
                     @endif
 
